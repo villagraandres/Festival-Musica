aca1cfc8f4620346c3b5430e04ac506e91c5bfca
Andrés Nehuel <villagraandres00@gmail.com>
2021-10-12 17:49:19 -0500

    Convirtiendo imagenes
    Agregue dos tipos de imagenes avif y webp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
-const {src,dest,watch}=require('gulp')
+const {src,dest,watch, parallel}=require('gulp')
+//css
 const sass=require('gulp-sass')(require('sass'))
 const plumber=require('gulp-plumber');
+
+// imagenes
+const cache=require('gulp-cache')
+const imagemin=require('gulp-imagemin')
+const webp=require('gulp-webp');
+const avif=require('gulp-avif')
+  
 
 function css(done){
     src('src/scss/**/*.scss')
@@ -13,6 +21,35 @@ function dev(done){
     watch('src/scss/**/*.scss',css)
     done();
 }
-
+function versionWebp(done){
+    const opciones ={
+        quality:50
+    };
+    src('src/img/**/*.{png,jpg}')
+    .pipe(webp(opciones))
+    .pipe(dest('build/img'))
+    done();
+}
+function imagenes(done){
+    const opciones={
+        optimizationLevel:3
+    }
+    src('src/img/**/*.{png,jpg}')
+    .pipe(cache(imagemin(opciones)))
+    .pipe(dest('build/img'))
+    done();
+}
+function versionAvif(done){
+    const opciones ={
+        quality:50
+    };
+    src('src/img/**/*.{png,jpg}')
+    .pipe(avif(opciones))
+    .pipe(dest('build/img'))
+    done();
+}
 exports.css=css;
-exports.dev=dev;
+exports.imagenes=imagenes;
+exports.versionWebp=versionWebp;
+exports.versionAvif=versionAvif;
+exports.dev= parallel(imagenes,versionWebp,versionAvif,dev);
